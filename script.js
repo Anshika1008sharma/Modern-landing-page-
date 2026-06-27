1a7425aa10acf20f399fcf458e10757321dc2374
Anshika1008sharma <anshika2703sharma@gmail.com>
2026-06-27 12:09:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function validateForm(){
+
+let name=document.getElementById("name").value;
+
+let email=document.getElementById("email").value;
+
+if(name=="" || email==""){
+
+alert("Please fill all required fields.");
+
+return false;
+
+}
+
+alert("Form Submitted Successfully!");
+
+return true;
+
+}
